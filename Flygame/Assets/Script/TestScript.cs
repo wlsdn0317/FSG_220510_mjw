2571456474032eebe0a6decec9d4c4c4871b6e7b
wlsdn0317 <78064428+wlsdn0317@users.noreply.github.com>
2022-05-10 21:04:24 +0900

    프로젝트 테스트코드 출력완료
    정상출력 확인

---
+++ Flygame/Assets/Script/TestScript.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("Hello Unity World");       
+    }
+
+}
